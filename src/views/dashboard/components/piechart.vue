d0df4cc86279fe8b01abf8ac9cf7201b91e6b2a9
quanshi <1435690979@qq.com>
2023-02-12 20:11:27 +0800

    主页

---
+++ src/views/dashboard/components/piechart.vue
@@ -0,0 +1,76 @@
+<template>
+  <!-- 雷达图  图表必须给高和宽度-->
+  <div ref="myDiv" class="radar-echart" />
+</template>
+
+<script>
+import * as echarts from 'echarts/core'
+import { ToolboxComponent, LegendComponent } from 'echarts/components'
+import { PieChart } from 'echarts/charts'
+import { LabelLayout } from 'echarts/features'
+import { CanvasRenderer } from 'echarts/renderers'
+
+// 注册必须的组件
+echarts.use([
+  ToolboxComponent,
+  LegendComponent,
+  PieChart,
+  CanvasRenderer,
+  LabelLayout
+])
+
+export default {
+  // 页面渲染完毕事件
+  mounted() {
+    const myChart = echarts.init(this.$refs.myDiv) // 得到图表实例
+    myChart.setOption({
+      // legend: {
+      //   top: 'bottom'
+      // },
+      toolbox: {
+        show: true,
+        feature: {
+          mark: { show: true },
+          dataView: { show: true, readOnly: false },
+          restore: { show: true },
+          saveAsImage: { show: true }
+        }
+      },
+      series: [
+        {
+          name: 'Nightingale Chart',
+          type: 'pie',
+          // radius: [50, 250],
+          // center: ['50%', '50%'],
+          roseType: 'area',
+          itemStyle: {
+            borderRadius: 8
+          },
+          data: [
+            { value: 40, name: 'rose 1' },
+            { value: 38, name: 'rose 2' },
+            { value: 32, name: 'rose 3' },
+            { value: 30, name: 'rose 4' },
+            { value: 28, name: 'rose 5' },
+            { value: 26, name: 'rose 6' },
+            { value: 22, name: 'rose 7' },
+            { value: 18, name: 'rose 8' }
+          ]
+        }
+      ]
+    })
+  }
+}
+</script>
+
+<style>
+.radar-echart {
+  position: relative;
+  width: 250px;
+  height: 150px;
+  padding: 0px;
+  margin: 0px;
+  border-width: 0px;
+  cursor: default;
+}
+</style>
